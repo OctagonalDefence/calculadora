ead4605944bb8fb93de6ae09364500b1fe87e62e
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-09-17 17:26:06 +0200

    m2

--- calculadora.py
+++ calculadora.py
@@ -6,6 +6,11 @@ resultat = int
 def suma(num1, num2): 
     return num1 + num2
 
+# Funció per a la resta
+# Definim la funció resta que rep dos números i retorna la seva resta
+# num1: primer número
+# num2: segon número
+
 def resta(num1, num2):
     return num1 - num2
 
@@ -14,6 +19,9 @@ def multiplicacio(num1, num2):
 
 def divisio(num1, num2):
     return num1 / num2
+
+def potencia(num1, num2):
+    return num1 ** num2
 
 def calculadora(num1, num2, operador):
     if operador == "+":
@@ -24,13 +32,15 @@ def calculadora(num1, num2, operador):
         return multiplicacio(num1, num2)
     elif operador == "/":
         return divisio(num1, num2)
+    elif operador == "^":
+        return potencia(num1, num2)
     else:
         return "Operador no vàlid"
 
 print("Benvingut a la calculadora!")   
 num1 = int(input("Introdueix el primer número: "))
 num2 = int(input("Introdueix el segon número: "))
-operador = input("Introdueix l'operador (+, -, *, /): ")
+operador = input("Introdueix l'operador (+, -, *, /, ^): ")
 
 resultat = calculadora(num1, num2, operador)
 print("El resultat és:", resultat) 
