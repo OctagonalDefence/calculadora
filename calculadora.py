06e9d2ee53ba4b8d26d778394db3f87882e3ca7d
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-09-17 17:19:10 +0200

    m2

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,37 @@
 num1 = int
 num2 = int
 operador = str
+resultat = int
+
+def suma(num1, num2): 
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacio(num1, num2):
+    return num1 * num2
+
+def divisio(num1, num2):
+    return num1 / num2
+
+def calculadora(num1, num2, operador):
+    if operador == "+":
+        return suma(num1, num2)
+    elif operador == "-":
+        return resta(num1, num2)
+    elif operador == "*":
+        return multiplicacio(num1, num2)
+    elif operador == "/":
+        return divisio(num1, num2)
+    else:
+        return "Operador no vàlid"
+
+print("Benvingut a la calculadora!")   
+num1 = int(input("Introdueix el primer número: "))
+num2 = int(input("Introdueix el segon número: "))
+operador = input("Introdueix l'operador (+, -, *, /): ")
+
+resultat = calculadora(num1, num2, operador)
+print("El resultat és:", resultat) 
 
